c3e606b9a409ae7d04cf0b40413ba1a0b467afd4
Primers-cern <Primers_cern@outlook.com>
2017-11-17 18:35:04 +0800

    Revert "17-Nov"
    This reverts commit a740c4004d68db0933055e5e49c89008e85ab4d1.

--- ballTime/main.js
+++ ballTime/main.js
@@ -1,77 +1,29 @@
-var WINDOW_WIDTH = document.body.clientWidth
-var WINDOW_HEIGHT = document.body.clientHeight
-var ran, marginL, marginT, ct
-var nowTime = new Date()
-
-var balls = []
-
-
+var ran, marginL, marginT
 
 window.onload = function () {
 	var canvas = document.getElementById('canvas')
-	ct = canvas.getContext('2d')
+	var context = canvas.getContext('2d')
 
-	canvas.width = WINDOW_WIDTH - 10  //减10抵消滚动条的宽度。
-	canvas.height = WINDOW_HEIGHT - 10 //避免出现滚动条
-
-	ran = parseInt(canvas.width/116-1) //根据画布宽度设定半径
+	canvas.width = 1000
+	canvas.height = 300
+	ran = parseInt(canvas.width/116-1)
     marginL = (canvas.width - 116*(ran+1) + 2*ran)/2
-    marginT = (canvas.height - 20*(ran+1))/2   //计算时钟居中显示
+    marginT = (canvas.height - 20*(ran+1))/2
 
-    setInterval(function () {
-        arrange()
-        update()
-    }, 50);
+    arrange(context)
+    setInterval(arrange, 1000, context);
 }
-
-function update () {
-    nextTime = new Date()
-    var nextHours = nextTime.getHours()
-    var nextMinutes = nextTime.getMinutes()
-    var nextSeconds =  nextTime.getSeconds()
-
-    var nowHours = nowTime.getHours()
-    var nowMinutes = nowTime.getMinutes()
-    var nowSeconds =  nowTime.getSeconds()
-
-    if (parseInt(nextHours/10) != parseInt(nowHours/10)) {
-        
-    }
-    if (parseInt(nextHours%10) != parseInt(nowHours%10)) {
-
-    }
-    if (parseInt(nextMinutes/10) != parseInt(nowMinutes/10)) {
-
-    }
-    if (parseInt(nextMinutes%10) != parseInt(nowMinutes%10)) {
-        
-    }
-    if (parseInt(nextSeconds/10) != parseInt(nowSeconds/10)) {
-        console.log("ten seconds pass")
-    }
-    if (parseInt(nextSeconds%10) != parseInt(nowSeconds%10)) {
-        console.log("one second pass")
-    }
-    nowTime = nextTime
-}
-
-function addBall () {
-    balls.push()
-}
-
 
 
 //生成画面数字
-function draw (loca, num) {
-    ct.fillStyle = "#0088ff"
-
+function draw (marginL, num, ct) {
 	var i, j
 	for (i=0; i<digit[num].length; i++) {
 		for (j=0; j<digit[num][i].length; j++) {
 			if (digit[num][i][j] == 1) {
 				ct.beginPath()
-				ct.arc(loca+(2*j+1)*(ran+1), marginT+(2*i+1)*(ran+1), ran, 0, 2*Math.PI)
-				
+				ct.arc(marginL+(2*j+1)*(ran+1), marginT+(2*i+1)*(ran+1), ran, 0, 2*Math.PI)
+				ct.fillStyle = "#0088ff"
 				ct.fill()
 			}
 		}
@@ -79,26 +31,22 @@ function draw (loca, num) {
 }
 
 //处理显示什么
-function arrange () {
-    var seconds
-    var nowSeconds = nowTime.getSeconds()
+function arrange (ct) {
+    var now = new Date()
+	var hours = now.getHours()
+	var minutes = now.getMinutes()
+	var seconds =  now.getSeconds()
 
-    if (seconds != nowSeconds) { //避免每帧渲染，只有当秒数变化时才渲染。
-        var hours = nowTime.getHours()
-        var minutes = nowTime.getMinutes()
-        var seconds =  nowTime.getSeconds()
+     ct.clearRect(0, 0, canvas.width, canvas.height)
 
-        ct.clearRect(0, 0, canvas.width, canvas.height)
-
-        draw(marginL, parseInt(hours/10))//小时十位
-        draw(marginL+16*(ran+1), parseInt(hours%10))//小时个位
-        draw(marginL+32*(ran+1), 10)//冒号
-        draw(marginL+42*(ran+1), parseInt(minutes/10))//分钟十位
-        draw(marginL+58*(ran+1), parseInt(minutes%10))//分钟个位
-        draw(marginL+74*(ran+1), 10)//冒号
-        draw(marginL+84*(ran+1), parseInt(seconds/10))//秒钟十位
-        draw(marginL+100*(ran+1), parseInt(seconds%10))//秒钟个位
-    }
+	draw(marginL, parseInt(hours/10), ct)//小时十位
+	draw(marginL+16*(ran+1), parseInt(hours%10), ct)//小时个位
+	draw(marginL+32*(ran+1), 10, ct)//冒号
+	draw(marginL+42*(ran+1), parseInt(minutes/10), ct)//分钟十位
+	draw(marginL+58*(ran+1), parseInt(minutes%10), ct)//分钟个位
+	draw(marginL+74*(ran+1), 10, ct)//冒号
+	draw(marginL+84*(ran+1), parseInt(seconds/10), ct)//秒钟十位
+	draw(marginL+100*(ran+1), parseInt(seconds%10), ct)//秒钟个位
 }
 
 
